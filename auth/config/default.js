1aaa0b9868a8c4a857b6b6221e6e8e200aae8d1e
Tony <nhat.phan@ntworld.net>
2018-03-07 20:41:00 +0700

    Initial Auth example

---
+++ auth/config/default.js
@@ -0,0 +1,9 @@
+const Path = require('path')
+
+module.exports = {
+  cwd: Path.join(__dirname, '../'),
+  port: 3000,
+  cookie: {
+    secret: 'test',
+  }
+}
---
+++ auth/index.ts
@@ -0,0 +1,23 @@
+import './autoload'
+import * as Path from 'path'
+import Najs, { ExpressHttpDriverServiceProvider, MongooseServiceProvider, HandlebarsViewServiceProvider } from 'najs'
+
+Najs.workingDirectory(Path.join(__dirname, '..'))
+  .classes('./autoload')
+  .providers([
+    /*
+    * Najs Framework Service Providers...
+    */
+    ExpressHttpDriverServiceProvider,
+    MongooseServiceProvider,
+    HandlebarsViewServiceProvider.withHandlebarsHelpers()
+
+    /*
+    * Package Service Providers...
+    */
+
+    /*
+    * Application Service Providers...
+    */
+  ])
+  .start()
